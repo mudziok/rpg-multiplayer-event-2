509a49bdc4c0cdd85a5dcdf692da2f51aba3b7ef
Pojemnik <andrzej.gauza@outlook.com>
2024-03-24 17:14:53 +0100

    Improve tree cutting minigame

--- Assets/Scripts/Minigames/MinigameBase.cs
+++ Assets/Scripts/Minigames/MinigameBase.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinigameBase : MonoBehaviour
 {
@@ -22,5 +24,28 @@ public class MinigameBase : MonoBehaviour
     protected void PerformAction()
     {
         actionPerformedEvent?.Invoke();
+    }
+
+    protected IEnumerator RotationCoroutine(Transform rect, Vector3 target, float speed, Action onEnd)
+    {
+        Quaternion targetRotation = Quaternion.Euler(target);
+        while (targetRotation != rect.rotation)
+        {
+            rect.rotation = Quaternion.RotateTowards(rect.rotation, targetRotation, speed * Time.deltaTime);
+            yield return null;
+        }
+        onEnd();
+    }
+
+    protected enum FadeDirection
+    {
+        FadeOut,
+        FadeIn
+    }
+    protected IEnumerator ImageFadeCoroutine(Image image, float duration, FadeDirection direction, Action onEnd)
+    {
+        image.CrossFadeAlpha(direction == FadeDirection.FadeOut ? 0 : 1, duration, false);
+        yield return new WaitForSeconds(duration);
+        onEnd();
     }
 }
--- Assets/Scripts/Minigames/WoodMinigame.cs
+++ Assets/Scripts/Minigames/WoodMinigame.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using UnityEditor.U2D;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WoodMinigame : MinigameBase
 {
+    //Current state of the minigame. It should change in the following order:
+    //Cutting->Falling->FadeOut->FadeIn
+    private enum State
+    {
+        Cutting,
+        Falling,
+        FadeOut,
+        FadeIn
+    }
+
     [Header("References")]
     [SerializeField]
     private RectTransform saw;
+    [SerializeField]
+    private Image tree;
 
     [Header("Config")]
     [SerializeField]
@@ -16,6 +29,12 @@ public class WoodMinigame : MinigameBase
     [SerializeField]
     [Tooltip("Where mouse movement detection ends in X axis, normalized to screen width")]
     private float mouseBorder;
+    [SerializeField]
+    [Tooltip("In degees per second")]
+    private float treeFallingSpeed;
+    [SerializeField]
+    [Tooltip("In seconds")]
+    private float treeFadeDuration;
 
     [SerializeField]
     [ReadOnly]
@@ -23,10 +42,19 @@ public class WoodMinigame : MinigameBase
     [SerializeField]
     [ReadOnly]
     private float mouseDistance;
+    [SerializeField]
+    [ReadOnly]
+    private State state;
 
     private void Awake()
     {
         gameObject.SetActive(false);
+        closedEvent += OnClose;
+    }
+
+    private void OnClose()
+    {
+        StopAllCoroutines();
     }
 
     private void Update()
@@ -36,12 +64,19 @@ public class WoodMinigame : MinigameBase
             Close();
         }
 
-        UpdateSawPosition();
-        UpdateMouseDistance();
-        if (mouseDistance > movementPerTree)
+        if (state == State.Cutting)
         {
-            PerformAction();
-            mouseDistance -= movementPerTree;
+            UpdateSawPosition();
+            UpdateMouseDistance();
+            if (mouseDistance > movementPerTree)
+            {
+                PerformAction();
+                mouseDistance = 0;
+                state = State.Falling;
+                saw.gameObject.SetActive(false);
+                //All of this could have been done using animations, but I've forgotten they exists
+                StartCoroutine(RotationCoroutine(tree.rectTransform, new Vector3(0, 0, 90), treeFallingSpeed, OnTreeFallEnd));
+            }
         }
     }
 
@@ -50,7 +85,16 @@ public class WoodMinigame : MinigameBase
         base.Open();
 
         lastMouseX = GetNormalizedAndClampedMouseX();
+        ResetMinigameState();
+    }
+
+    private void ResetMinigameState()
+    {
         mouseDistance = 0;
+        state = State.Cutting;
+        saw.gameObject.SetActive(true);
+        tree.CrossFadeAlpha(1, 0, false);
+        tree.rectTransform.rotation = Quaternion.identity;
     }
 
     private void UpdateSawPosition()
@@ -71,5 +115,24 @@ public class WoodMinigame : MinigameBase
     {
         float normalized = Input.mousePosition.x / Screen.width;
         return Mathf.Clamp(normalized, 0.5f - mouseBorder, 0.5f + mouseBorder);
+    }
+
+    private void OnTreeFallEnd()
+    {
+        state = State.FadeOut;
+        StartCoroutine(ImageFadeCoroutine(tree, treeFadeDuration, FadeDirection.FadeOut, OnTreeFadeOutEnd));
+    }
+
+    private void OnTreeFadeOutEnd()
+    {
+        state = State.FadeIn;
+        tree.rectTransform.rotation = Quaternion.identity;
+        StartCoroutine(ImageFadeCoroutine(tree, treeFadeDuration, FadeDirection.FadeIn, OnTreeFadeInEnd));
+    }
+
+    private void OnTreeFadeInEnd()
+    {
+        state = State.Cutting;
+        saw.gameObject.SetActive(true);
     }
 }
